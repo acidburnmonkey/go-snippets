cf98bb55a31ebbe016f3d4a5d1048240bd2fc2fb
Acid <acidburnmonkey@gmail.com>
2026-06-14 16:42:48 -0400

    modified:   jsonResponse.go

--- jsonResponse.go
+++ jsonResponse.go
@@ -1,6 +1,5 @@
 package main
 
-// pkg/response/json.go
 // package response
 
 import (
@@ -8,15 +7,15 @@ import (
 	"net/http"
 )
 
-// JSON() -> sends a json response with status code + data \n
-// ex: response.JSON(w, http.StatusOK, events)
-func JSON(w http.ResponseWriter, status int, data interface{}) {
+// JsonResponse() -> sends a json response with status code + data \n
+// ex: JsonResponse(w, http.StatusOK, events)
+func JsonResponse(w http.ResponseWriter, status int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(data)
 }
 
-// Error() -> sends a json response error + message
-func Error(w http.ResponseWriter, status int, message string) {
-	JSON(w, status, map[string]string{"error": message})
+// ErrorResponse() -> sends a json response error + message
+func ErrorResponse(w http.ResponseWriter, status int, message string) {
+	JsonResponse(w, status, map[string]string{"error": message})
 }
